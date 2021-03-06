f2e62e4d9cbb20b14c368e5c13ff7ab6f593501c
thinhnguyenphuc <thinhnguyenphuc55@gmail.com>
2021-03-07 00:34:13 +0700

    ! Photo details layout

--- src/app/src/main/java/com/example/photo_manager/MainActivity.java
+++ src/app/src/main/java/com/example/photo_manager/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.photo_manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button activity2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activity2 = (Button) findViewById(R.id.button_activity2);
+
+        activity2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Photo_Details.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
