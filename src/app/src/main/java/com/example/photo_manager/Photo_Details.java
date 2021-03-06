c7a9aafaa3f342075e633ca5d365eba2fd99d914
thinhnguyenphuc <thinhnguyenphuc55@gmail.com>
2021-03-06 22:33:17 +0700

    + Photo details layout

---
+++ src/app/src/main/java/com/example/photo_manager/Photo_Details.java
@@ -0,0 +1,14 @@
+package com.example.photo_manager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Photo_Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo__details);
+    }
+}
