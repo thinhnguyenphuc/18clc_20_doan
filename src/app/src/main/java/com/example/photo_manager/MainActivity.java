28db20f47d98cfc6102702748ada1ad9f385160d
thinhnguyenphuc <thinhnguyenphuc55@gmail.com>
2021-03-09 22:45:42 +0700

    ! Main Layout

--- src/app/src/main/java/com/example/photo_manager/MainActivity.java
+++ src/app/src/main/java/com/example/photo_manager/MainActivity.java
@@ -10,12 +10,22 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button activity2,activity3;
+    Button activity1,activity2,activity3;
     Button album_detail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activity1 = (Button) findViewById(R.id.button_activity);
+
+        activity1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, album_general.class);
+                startActivity(intent);
+            }
+        });
 
         activity2 = (Button) findViewById(R.id.button_activity2);
 
@@ -39,9 +49,12 @@ public class MainActivity extends AppCompatActivity {
 
         album_detail = findViewById(R.id.album_detail);
 
-        album_detail.setOnClickListener((v) -> {
-            Intent intent = new Intent(MainActivity.this, album_detail.class);
-            startActivity(intent);
+        album_detail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, album_detail.class);
+                startActivity(intent);
+            }
         });
     }
 }
