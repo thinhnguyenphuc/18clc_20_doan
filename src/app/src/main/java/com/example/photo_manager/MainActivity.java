1c6212b80e385cf869c8829d45daf1fd0ac4af73
thinhnguyenphuc <thinhnguyenphuc55@gmail.com>
2021-04-14 21:49:32 +0700

    + Take a new picture

--- src/app/src/main/java/com/example/photo_manager/MainActivity.java
+++ src/app/src/main/java/com/example/photo_manager/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.photo_manager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button activity1,activity2,activity3;
     Button album_detail;
+    int STORAGE_PERMISSION_CODE = 0;
+    int CAMERA_PERMISSION_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,5 +65,34 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        Button take_new_photo = (Button)findViewById(R.id.new_photo);
+        take_new_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAllPermission();
+                New_Photo new_photo = new New_Photo(MainActivity.this);
+            }
+        });
     }
+    private void checkAllPermission(){
+        checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE);
+        checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE);
+        checkPermission(Manifest.permission.CAMERA,CAMERA_PERMISSION_CODE);
+    }
+    public void checkPermission(String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permission)
+                == PackageManager.PERMISSION_DENIED) {
+
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] { permission },
+                    requestCode);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
 }
