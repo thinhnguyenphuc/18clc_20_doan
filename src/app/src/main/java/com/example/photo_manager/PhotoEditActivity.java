fe62566ace04df3d8bcc8dd77392d8aa868ae462
Nam Huynh Nhat <nnhuynh306@gmail.com>
2021-04-20 13:07:32 +0700

    + photo editor feature: filter

--- src/app/src/main/java/com/example/photo_manager/PhotoEditActivity.java
+++ src/app/src/main/java/com/example/photo_manager/PhotoEditActivity.java
@@ -3,11 +3,15 @@ package com.example.photo_manager;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -15,22 +19,27 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.divyanshu.colorseekbar.ColorSeekBar;
+import com.example.photo_manager.PEAdapters.PEFilterAdapter;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.slider.Slider;
+
+import java.util.ArrayList;
 
 import ja.burhanrashid52.photoeditor.OnPhotoEditorListener;
 import ja.burhanrashid52.photoeditor.PhotoEditor;
 import ja.burhanrashid52.photoeditor.PhotoEditorView;
+import ja.burhanrashid52.photoeditor.PhotoFilter;
 import ja.burhanrashid52.photoeditor.ViewType;
+
+import static ja.burhanrashid52.photoeditor.PhotoEditor.getEmojis;
 
 public class PhotoEditActivity extends AppCompatActivity {
 
-    AlertDialog text_dialog;
     PhotoEditor mPhotoEditor;
 
     ImageButton undo_button, redo_button, share_button, save_button;
 
-    ImageButton brush_button, eraser_button, text_button, filter_button, brightness_button;
+    ImageButton brush_button, eraser_button, text_button, filter_button, brightness_button, emoji_button;
 
     //brush properties
     float brush_size;
@@ -39,6 +48,8 @@ public class PhotoEditActivity extends AppCompatActivity {
 
     //eraser properties
     float eraser_size;
+
+    //filter properties
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +74,10 @@ public class PhotoEditActivity extends AppCompatActivity {
                 .setDefaultEmojiTypeface(mEmojiTypeFace)
                 .build();
 
+        for (PhotoFilter pf: PhotoFilter.values()) {
+            mPhotoEditor.setFilterEffect(pf);
+        }
+
         this.undo_button = findViewById(R.id.undo_btn);
         this.redo_button = findViewById(R.id.redo_btn);
         this.share_button = findViewById(R.id.share_btn);
@@ -73,6 +88,7 @@ public class PhotoEditActivity extends AppCompatActivity {
         this.text_button = findViewById(R.id.text_button);
         this.filter_button = findViewById(R.id.filter_button);
         this.brightness_button = findViewById(R.id.brightness_button);
+        this.emoji_button = findViewById(R.id.add_emoji_button);
 
         this.brush_size = Float.parseFloat(getString(R.string.brush_size_max));
         this.brush_opacity = Integer.parseInt(getString(R.string.brush_opacity_max));
@@ -188,7 +204,7 @@ public class PhotoEditActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 View view = getLayoutInflater().inflate(R.layout.pe_text_editor, null);
-                text_dialog = new AlertDialog.Builder(PhotoEditActivity.this).create();
+                final AlertDialog text_dialog = new AlertDialog.Builder(PhotoEditActivity.this).create();
 
                 final int[] color = new int[1];
 
@@ -232,7 +248,7 @@ public class PhotoEditActivity extends AppCompatActivity {
             @Override
             public void onEditTextChangeListener(final View rootView, String text, final int colorCode) {
                 View view = getLayoutInflater().inflate(R.layout.pe_text_editor, null);
-                text_dialog = new AlertDialog.Builder(PhotoEditActivity.this).create();
+                final AlertDialog text_dialog = new AlertDialog.Builder(PhotoEditActivity.this).create();
 
                 final int[] color = new int[1];
 
@@ -290,6 +306,32 @@ public class PhotoEditActivity extends AppCompatActivity {
             @Override
             public void onStopViewChangeListener(ViewType viewType) {
 
+            }
+        });
+
+        filter_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final BottomSheetDialog dialog = new BottomSheetDialog(PhotoEditActivity.this);
+
+                View filter_chooser_view = getLayoutInflater().inflate(R.layout.pe_filter_chooser, null);
+                LinearLayoutManager layoutManager
+                        = new LinearLayoutManager(PhotoEditActivity.this, LinearLayoutManager.HORIZONTAL, false);
+
+                RecyclerView myList = (RecyclerView) filter_chooser_view.findViewById(R.id.filter_list);
+                myList.setLayoutManager(layoutManager);
+                myList.setAdapter(new PEFilterAdapter(PhotoEditActivity.this, mPhotoEditor, PhotoFilter.values()));
+
+                dialog.setContentView(filter_chooser_view);
+                dialog.show();
+            }
+        });
+
+        emoji_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<String> unicodes = PhotoEditor.getEmojis(PhotoEditActivity.this);
+                mPhotoEditor.addEmoji(unicodes.get(0));
             }
         });
     }
