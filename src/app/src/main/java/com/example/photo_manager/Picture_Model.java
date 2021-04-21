d4a79c971a316e8978beaa1399509e0faec8f71a
thinhnguyenphuc <thinhnguyenphuc55@gmail.com>
2021-04-21 12:20:31 +0700

    ! Photo Details
    Add time to picture model
    Edit layout of Photo Detail

--- src/app/src/main/java/com/example/photo_manager/Picture_Model.java
+++ src/app/src/main/java/com/example/photo_manager/Picture_Model.java
@@ -2,16 +2,20 @@ package com.example.photo_manager;
 
 import android.net.Uri;
 
+import java.util.Date;
+
 public class Picture_Model {
 
     private Uri uri;
     private String name;
     private int size;
+    private String time;
 
 
-    public Picture_Model(Uri uri,String name, int size){
+    public Picture_Model(Uri uri,String name,String time, int size){
         this.uri = uri;
         this.name = name;
+        this.time = time;
         this.size = size;
     }
 
@@ -24,12 +28,14 @@ public class Picture_Model {
     public int getSize(){
         return size;
     }
+    public String getTime(){return time;}
     public void setUri(Uri uri){
         this.uri = uri;
     }
     public void setName(String name){
         this.name = name;
     }
+    public void setTime(String time){this.time = time;}
     public void setSize(int size){
         this.size = size;
     }
